a15be8e403d231eb1f073a0f796be113b90f1906
Cauã Marques <163948818+Claytinhooo@users.noreply.github.com>
2024-07-24 18:44:58 -0300

    Add files via upload

---
+++ Portfólio-Cauã/main.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".swiper", {
+    effect: "cube",
+    allowTouchMove:false,
+    grabCursor: false,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    mousewheel:true
+});
+swiper.sliderMove = function (s, e) {
+    console.log(s)
+}
+function Navigate(indx) {
+    for (let i of document.querySelectorAll(".Links li")) i.classList.remove("activeLink")
+    Array.from(document.querySelectorAll(".Links li"))[indx].classList.add("activeLink")
+    swiper.slideTo(indx, 1000, true)
+}
+
